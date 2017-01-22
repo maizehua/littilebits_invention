c04b24d2eb57d60a4b30c80109ebb771e9d703a1
Zehua Mai <mai.zehua@qq.com>
2017-01-22 03:25:31 -0500

    Save and show bits

--- app/controllers/inventions_controller.rb
+++ app/controllers/inventions_controller.rb
@@ -4,12 +4,27 @@ class InventionsController < ApplicationController
   end
 
   def create
+    # make sure we have bits for the invention
+    if params[:bits].present?
+      bits_list = params[:bits].split(/[\s,]+/).uniq
+      if bits_list.empty?
+        flash[:error] = "Please select at least one bit"
+        return redirect_to action: "new"
+      end
+    else
+      flash[:error] = "Please select at least one bit"
+      return redirect_to action: "new"
+    end
+
     invention = Invention.create({
       title: params[:title],
       description: params[:description],
       email: params[:email],
       username: params[:name],
     })
+
+    add_bits_to_invention(bits_list, invention)
+
     if params[:materials].present?
       material_list = params[:materials].split(/[\s,]+/)
       invention.material_list = material_list
@@ -20,8 +35,18 @@ class InventionsController < ApplicationController
 
   def show
     @invention = Invention.find(params[:id])
+    @bits_list = @invention.bits.map(&:name)
   end
 
   def new
+  end
+
+  private
+
+  def add_bits_to_invention(bits_list, invention)
+    bits_list.each do |bit_name|
+      bit = Bit.find_by_name(bit_name)
+      invention.bits << bit
+    end
   end
 end
